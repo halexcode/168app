a1614ce7aab852326ace7f64b4e4ca30a3fac8e5
halex <halexcode@163.com>
2016-10-17 10:02:23 +0800

    上传数据库到恒创独立主机168app

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -13,9 +13,9 @@ return array(
 		// 数据库配置信息
 	'DB_TYPE' => 'mysql', // 数据库类型
  	'DB_HOST' => 'localhost', // 服务器地址
-	'DB_NAME' => '168app', // 数据库名
-	'DB_USER' => 'root', // 用户名
- 	'DB_PWD' => 'hxb19850927', // 密码
+	'DB_NAME' => 'hkdulico_168app', // 数据库名
+	'DB_USER' => 'hkdulico', // 用户名
+ 	'DB_PWD' => 'hxb)927xyy', // 密码
 	'DB_PORT' => 3306, // 端口
  	'DB_PREFIX' => '168app_', // 数据库表前缀
  	'DB_CHARSET'=> 'utf8', // 字符集
